1950af2db07c9d6e9f757f552d7f24bdfe713c9c
chloeych <52624099+chloeych@users.noreply.github.com>
2020-12-03 15:03:33 +0800

    formatting

--- cc_finalproj/sketch.js
+++ cc_finalproj/sketch.js
@@ -14,11 +14,13 @@ let dreamtimeAudio;
 
 // declaring buttons
 let audioBtn;
+let audioCount = 0;
 
 function preload(){
   // Preload Images
-  for (let i=0; i<3; i++){ 
-    layer1[i] = loadImage("art_img/layer1/img_"+i+".png"); // Preload 'backgroundImg' Object 
+
+  layer1[0] = loadImage("art_img/layer1/img_0.png"); // Preload 'backgroundImg' Object 
+  for (let i=0; i<14; i++){ 
     layer2[i] = loadImage("art_img/layer2/img_"+i+".png"); // Preload Layer 2 Object 
   }
 
@@ -29,8 +31,8 @@ function preload(){
 
 function setup() {
   createCanvas(4000, 2000); 
-  for (let i=0; i<3; i++){
-    backgroundImg[i] = new StaticImg();
+  backgroundImg[0] = new StaticImg();
+  for (let i=0; i<14; i++){
     layerOneImg[i] = new MovingImg();
     
     // Audio Button Set Up 
@@ -56,27 +58,47 @@ function draw() {
     }
   }
 
-  // Display Static
-
-  backgroundImg[1].display([1], 300, 0);
 
   // Display and Setup of Parallax Images 
 
-  layerOneImg[0].parallax([0], 0, 200, 100, 200, 0, 400, 100, 800);
-  layerOneImg[1].parallax([1], 500, 800, 600, 1200, 0, 100, 200, 300);
-  layerOneImg[1].parallax([2], 300, 600, 200, 400, 1000, 1200, 800, 1000);
-  for (let i=0; i<3; i++) { // Display Images 
+  layerOneImg[0].parallax([0], 200, 250, 100, 500, 0, 700, 700, 800);
+  layerOneImg[1].parallax([1], 0, 100, 50, 200, 0, 100, 200, 300);
+  layerOneImg[2].parallax([2], 2800, 2900, 3000, 3200, 0, 100, 50, 300);
+  layerOneImg[3].parallax([3], 100, 1900, 200, 2200, 0, 100, 200, 300);
+  layerOneImg[4].parallax([4], 900, 1100, 1200, 1600, 0, 400, 0, 800);
+  layerOneImg[5].parallax([5], 800, 1000, 2000, 2200, 0, 100, 200, 300);
+  layerOneImg[6].parallax([6], 700, 800, 1000, 1400, 600, 900, 1100, 1400);
+  layerOneImg[7].parallax([7], 200, 600, 900, 1000, 0, 100, 200, 300);
+  layerOneImg[8].parallax([8], 2600, 2800, 3000, 3200, 500, 800, 1200, 1300);
+  layerOneImg[9].parallax([9], 500, 800, 600, 1200, 0, 100, 200, 300);
+  layerOneImg[10].parallax([10], 100, 120, 100, 120, 0, 20, 0, 30);
+  layerOneImg[11].parallax([11], 0, 50, 100, 200, 900, 1200, 1000, 1400);
+  layerOneImg[12].parallax([12], 2600, 2700, 2500, 2600, 600, 700, 600, 800);
+  layerOneImg[13].parallax([13],2700, 2900, 3200, 3500, 0, 100, 200, 300);
+
+  for (let i=0; i<14; i++) { // Display Images 
     layerOneImg[i].display([i],xScroll[i], yScroll[i] );
   }
 
   // Display Buttons 
   audioBtn.draw();
   audioBtn.onPress = function(){
-    console.log("pressed");
-    dreamtimeAudio.play();
+    audioCount += 1; 
+    console.log(audioCount);
+
+    if(audioCount == 1){
+      dreamtimeAudio.play();
+    }
+
+    if(audioCount == 2){
+      dreamtimeAudio.stop();
+    }
+
+    if(audioCount == 3){
+      audioCount = 1;
+      dreamtimeAudio.play();
+    }
   }
-
-
 }
 
 class StaticImg{
@@ -126,6 +148,12 @@ class MovingImg{
         // https://www.aboriginalartstore.com.au/artists/malcolm-maloney-jagamarra/lander-river/
         // https://www.aboriginalartstore.com.au/artists/paddy-fordham-wainburranga/mermaid-sisters/
         // https://www.aboriginalartstore.com.au/artists/malcolm-maloney-jagamarra/inapaku-dreaming/
+        // https://www.hiclipart.com/free-transparent-background-png-clipart-pvqyd
+        // https://theupsider.com.au/the-national-2019/9814
+        // https://www.artnews.com/gallery/art-in-america/aia-photos/history-wars-aboriginal-art/
+        // https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mca.com.au%2Fartists-works%2Fartists%2Fgordon-bennett%2F&psig=AOvVaw1wGMaGMN45pGaPCb0WgEQb&ust=1607062181913000&source=images&cd=vfe&ved=2ahUKEwiVg5Olk7HtAhVjkOYKHVQwAVcQjhx6BAgAEA4
+        // https://books.openedition.org/obp/8107
+
     // Audio 
         // https://www.youtube.com/watch?v=xDJrnldb08o
 
