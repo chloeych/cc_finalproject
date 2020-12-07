e215cc7b03cc12813d079a88b6bbc726fa256e02
chloeych <52624099+chloeych@users.noreply.github.com>
2020-12-07 23:34:09 +0800

    more images added

--- cc_finalproj/sketch.js
+++ cc_finalproj/sketch.js
@@ -18,10 +18,15 @@ let x1, x2, y1, y2; //Variables for Parallax Locations
 
 // declaring audio
 let dreamtimeAudio;
+let dreamtimeAudio1;
 
 // declaring buttons
 let audioBtn;
+let audioBtn1;
+let audioBtn2;
 let audioCount = 0;
+let audio1Count = 0;
+let audio2Count = 0;
 let xBtn; 
 let xBtnCount = 0; 
 
@@ -34,21 +39,22 @@ function preload(){
   // Preload Images
 
   layer1[0] = loadImage("art_img/layer1/img_0.png"); // Preload 'backgroundImg' Object 
-  for (let i=0; i<14; i++){ 
+  for (let i=0; i<19; i++){ 
     layer2[i] = loadImage("art_img/layer2/img_"+i+".png"); // Preload Layer 2 Object 
   }
 
   // Preload Audio 
   soundFormats('mp3');
   dreamtimeAudio = loadSound('audio/dreamtime_0.mp3');
-
+  dreamtimeAudio1 = loadSound('audio/dreamtime_1.mp3');
+  dreamtimeAudio2 = loadSound('audio/dreamtime_2.mp3');
 
 }
 
 function setup() {
   createCanvas(4000, 2000); 
   backgroundImg[0] = new StaticImg();
-  for (let i=0; i<14; i++){
+  for (let i=0; i<19; i++){
     layerOneImg[i] = new MovingImg();
     
     // Popup Button Set Up 
@@ -63,7 +69,8 @@ function setup() {
 
     // Audio Button Set Up 
     audioBtn = new Clickable();
-    audioBtn.resize(100, 50); 
+    audioBtn1 = new Clickable();
+    audioBtn2 = new Clickable();
   
   }
 }
@@ -98,13 +105,24 @@ function draw() {
   layerOneImg[11].parallax([11], 0, 50, 100, 200, 900, 1200, 1000, 1400);
   layerOneImg[12].parallax([12], 2800, 2900, 3100, 3200, 400, 500, 500, 600);
   layerOneImg[13].parallax([13],2700, 2900, 3200, 3500, 0, 100, 200, 300);
+  layerOneImg[14].parallax([14],2000, 2300, 2400, 2900, 400, 600, 500, 800);
+  layerOneImg[15].parallax([15], 700, 900, 800, 1100, 550, 700, 650, 900);
+  layerOneImg[16].parallax([16],0, 300, 50, 500, 0, 50, 300, 600);
+  layerOneImg[17].parallax([17],1900, 2600, 2300, 2900, 600, 700, 650, 800);
+  layerOneImg[18].parallax([18],300, 600, 400, 700, 0, 100, 0, 100);
 
-  for (let i=0; i<14; i++) { // Display Images 
+  for (let i=0; i<19; i++) { // Display Images 
     layerOneImg[i].display([i],xScroll[i], yScroll[i] );
   }
 
   // Display Buttons 
   audioBtn.draw();
+  audioBtn.textFont = "Antic Slab";
+  audioBtn.color = '#702c28';
+  audioBtn.strokeWeight = 0;
+  audioBtn.locate(1080,580);
+  audioBtn.resize(300,50);
+  audioBtn.text = "Mirram The Kangaroo and Warreen The Wombat";
   audioBtn.onPress = function(){
     audioCount += 1; 
     console.log(audioCount);
@@ -122,6 +140,54 @@ function draw() {
       dreamtimeAudio.play();
     }
   }
+
+  audioBtn1.draw();
+    audioBtn1.textFont = "Antic Slab";
+    audioBtn1.color = '#702c28';
+    audioBtn1.strokeWeight = 0;
+    audioBtn1.locate(2500,800);
+    audioBtn1.resize(300,50);
+    audioBtn1.text = "How The Kangaroo Got It's Pouch";
+    audioBtn1.onPress = function(){
+      audio1Count += 1; 
+  
+      if(audio1Count == 1){
+        dreamtimeAudio1.play();
+      }
+  
+      if(audio1Count == 2){
+        dreamtimeAudio1.stop();
+      }
+  
+      if(audio1Count == 3){
+        audio1Count = 1;
+        dreamtimeAudio1.play();
+      }
+}
+
+audioBtn2.draw();
+    audioBtn2.textFont = "Antic Slab";
+    audioBtn2.color = '#702c28';
+    audioBtn2.strokeWeight = 0;
+    audioBtn2.locate(3200,1800);
+    audioBtn2.resize(300,50);
+    audioBtn2.text = "Wayambeh The Turtle";
+    audioBtn2.onPress = function(){
+      audio2Count += 1; 
+  
+      if(audio2Count == 1){
+        dreamtimeAudio2.play();
+      }
+  
+      if(audio2Count == 2){
+        dreamtimeAudio2.stop();
+      }
+  
+      if(audio2Count == 3){
+        audio2Count = 1;
+        dreamtimeAudio2.play();
+      }
+}
 
   //acknowledgement pop up 
   fill(0,outBoxOpacity);
@@ -201,7 +267,15 @@ class MovingImg{
         // https://www.artnews.com/gallery/art-in-america/aia-photos/history-wars-aboriginal-art/
         // https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mca.com.au%2Fartists-works%2Fartists%2Fgordon-bennett%2F&psig=AOvVaw1wGMaGMN45pGaPCb0WgEQb&ust=1607062181913000&source=images&cd=vfe&ved=2ahUKEwiVg5Olk7HtAhVjkOYKHVQwAVcQjhx6BAgAEA4
         // https://books.openedition.org/obp/8107
+        // https://www.aboriginalsignature.com/art-aborigene-papunya-tula/ronnie-tjampitjinpa-site-of-walungurru-ancestral-stories
+        // https://www.nytimes.com/2011/11/05/arts/05iht-rartabor05.html
+        // https://www.aboriginalartstore.com.au/artists/paddy-fordham-wainburranga/married-man/
+        // http://www.outstation.com.au/stockroom/carlene-thompson-tjulpu-kulunypa/
+        // https://www.artery.com.au/index.php?option=com_shop&view=product&catalogue=11575
+
 
     // Audio 
-        // https://www.youtube.com/watch?v=xDJrnldb08o
+        // https://www.youtube.com/watch?v=xDJrnldb08o Mirram The Kangaroo and Warreen The Wombat
+        // https://www.youtube.com/watch?v=8sWFAGGWvUA How the Kangaroo Got It's Pouch
+        // https://www.youtube.com/watch?v=DpzDvpZ0hMg&t=1s Wayambeh The Turtle
 
